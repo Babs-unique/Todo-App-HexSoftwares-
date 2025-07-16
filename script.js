5a25f1eb4dbf72c333177f1389f8ad9f3c5ca937
Babs-unique <145204996+Babs-unique@users.noreply.github.com>
2025-07-16 13:28:41 +0100

    Edited and refined My code
    Refining and editing of my code

---
+++ script.js
@@ -0,0 +1,85 @@
+let toggleBtn = document.getElementById("toggleBtn");
+let toggleMode = document.getElementById("toggleMode");
+
+//LIGHT-MODE TOGGLE
+toggleBtn.addEventListener("click",()=>{
+    let body = document.querySelector("body");
+    toggleMode.innerHTML = `<img src="./images/icon-sun.svg" alt="Light Mode Toggle" id="toggleMode">`
+    body.classList.toggle("light-mode");
+    if (body.className === "light-mode") {
+            toggleMode.src= "./images/icon-moon.svg"
+    }
+    else{
+        toggleMode.src= "./images/icon-sun.svg"
+    }
+});
+
+
+
+
+//GET VALUE FROM THE INPUT BOX
+
+let inputBox = document.getElementById("input");
+
+//ADD TODO
+function addTodo(event){
+    if (event.key === "Enter") {
+        console.log(inputBox.value);
+        let container = document.getElementById("todo-container");
+        let listItems = document.createElement("div");
+        listItems.className = "list_items";
+        listItems.innerHTML = `<div class="chh">
+                                <input type="checkbox" name="" id="checkbox" class="completed">
+                            <span class="text">${inputBox.value}</span>
+                            </div>
+                            <div class="cr">
+                                <img src="./images/icon-cross.svg" alt="Cancel todo" id="cancel-todo" class="cancel-btn">
+                            </div>`
+                container.appendChild(listItems);
+
+            
+            //DELETE TODO
+            /*     let container = document.getElementById("todo-container"); */
+                const cancelBtn = listItems.querySelector(".cancel-btn")
+        cancelBtn.addEventListener('click',()=>{
+                listItems.remove();
+                countedTodo();
+        });
+        //MARK COMPLETED TODO
+        const completedTodo = listItems.querySelector(".completed");
+        let spanElement = listItems.querySelector(".text");
+        completedTodo.addEventListener('click',()=>{
+            console.log("checked");
+            spanElement.classList.toggle("line-through");
+        });
+        //INCREMENT AND COUNT TODO
+        countedTodo();
+        //REMOVE TEXT IN TE INPUT BOX AFTER CLICKING ENTER
+        inputBox.value = "";
+    }
+}
+//EVENT LISTENER FOR OUR ADD TODO AND CALLING OF OUR FUNCTION
+inputBox.addEventListener("keydown",addTodo);
+
+//COUNT TODO
+let todoCount = document.getElementById("countTodo");
+function countedTodo(){
+    let todoItem = document.querySelectorAll(".list_items")
+    todoCount.innerHTML = `<p>${todoItem.length} items left</p>`;
+}
+
+//CLEAR ALL COMPLETED TODO
+let clearCompleted = document.getElementById("clearComplete");
+    clearCompleted.addEventListener('click',()=>{
+    const completedTodos = document.querySelectorAll(".list_items");
+    completedTodos.forEach((listItem)=>{
+        let todoComplete = listItem.querySelector(".text")
+        if (todoComplete.classList.contains("line-through")){
+            listItem.remove();
+        }
+    })
+    countedTodo();
+});
+
+//FILTER BETWEEN COMPLETED AND ACTIVE TODO'S
+
